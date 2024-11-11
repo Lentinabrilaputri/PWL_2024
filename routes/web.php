9a0395d62ebce996682560844ead64d672ea3ee3
Lentinabrilaputri <lentinaabrilaputri.com>
2024-11-11 11:33:31 +0700

    Praktikum 1

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/about', function () {
+    return '<p>NIM: 2241760120</p><p>Nama: Lentina Abrila Putri</p>';
+});
+
+// Route::get('/user/{Lentina}', function ($name) {
+//     return 'Nama saya: '.$name;
+// });
+
+// Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+//     return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+// });
+
+// Route::get('/articles/{id}', function ($id) {
+//     return "Halaman Artikel dengan ID " . $id;
+// });
+
+// Route::get('/user/{name?}', function ($name=null) {
+//     return "Nama saya Lentina " . $name;
+// });
+
+Route::get('/user/{name?}', function ($name='Lentina') { return 'Nama saya '.$name;
 });
